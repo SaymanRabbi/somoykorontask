30eca14c8a18abb3266b2b68b05c4c025e1282e1
saymanrabbi <mdrabbi.asm@gmail.com>
2023-07-07 14:18:43 +0600

    page layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
 
 export default function Home() {
   return (
-    <h2>
-      hello
-    </h2>
+   <div className="w-[100%] h-[100vh]">
+    <div className="max-h-full h-full md:h-auto md:max-h-[90vh] w-full md:w-[90vw] md:max-w-[450px] rounded-md p-[25px] focus:outline-none">
+        
+    </div>
+
+   </div>
   )
 }
